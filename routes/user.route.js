946a30d1e3408fc8604c266df455ecaa4767970e
Habeeb Lasisi <lasisihabeeb.lh@gmail.com>
2025-05-30 22:42:16 +0100

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getUser, createUser, updateUser, deleteUser } = require('../controllers/user.controler');
+const { loginUser, getUser, createUser, updateUser, deleteUser } = require('../controllers/user.controller');
 
 const route = express.Router();
 
 route.get('/user', getUser);
 
 route.post('/user', createUser);
+
+route.post('/user/login', loginUser);
 
 route.put('/user', updateUser);
 
